03d1688688aa9e3b12ca661b16693872558a07fb
Sandra Sanders <sandy-sanders@msn.com>
2021-10-18 17:17:27 -0700

    cards and header started css git push origin feature/layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,5 @@
+ var apiKey ="bd08044f4b83ef10ec4cb21575356c6f"
 
-
+var weather= "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apiKey}"
+    console.log(weather)
+    
