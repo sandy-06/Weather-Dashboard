229d33d12a83f8aca5c11e50530b82db67c197b5
Sandra Sanders <sandy-sanders@msn.com>
2021-10-20 18:59:35 -0700

    urlfunction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
  
-var cityTextArea = document.getElementsById("city")
+var cityTextArea = document.getElementById("city");
 
 var searchCityBtn = document.getElementsByClassName("cityButton");
 
@@ -7,8 +7,26 @@ var requestURL="https://api.openweathermap.org/data/2.5/forecast?q="
 var apiKey ="bd08044f4b83ef10ec4cb21575356c6f"
 
 //get city
-searchCityBtn.addEventListener("click", function() {
+searchCityBtn[0].addEventListener("click", function() {
+    var city = cityTextArea.value;
+    fetch(`${requestURL}${city}&appid=${apiKey}`).then(function (res){
+        return res.json()
+    }).then(function (res){
+        console.log(res)
+      var lat = res.city.coord.lat;
+      var lon = res.city.coord.lon;
+        var multiDay=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`
+fetch(multiDay).then(function (res){
+    return res.json()
 
+}).then(function (data){
+    console.log(data)
+})
+    })
+
+var fiveDay = function (res) {
+    
+}
 })
 
 
